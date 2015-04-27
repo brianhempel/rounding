9f7a9974faacf150cead0e72e7b06bf118f9d364
Brian Hempel <bnhempel@mtu.edu>
2015-04-27 13:39:21 -0400

    Fix float rounding errors with ActiveSupport::Duration
    Works around this problem:
    
    rational
    => (1430438399999999999/1000000000)
    rational % 1
    => (999999999/1000000000)
    rational % 1.second
    => (-1/1000000000)
    
    Which produced incorrect rounding:
    
    Time.current.end_of_month
    => Thu, 30 Apr 2015 23:59:59 UTC +00:00
    Time.current.end_of_month.floor_to(1.second)
    => Fri, 01 May 2015 00:00:00 UTC +00:00
    
    Root of the problem is in Rational#/
    
    https://github.com/ruby/ruby/blob/f5a55037022a11fdc707b782295505b44c745322/rational.c#L897-L944
    
    `1.second` should count as a Fixnum, but it seems that
    `RB_TYPE_P(other, T_FIXNUM)` returns false for it (presumably due to
    boxing), so the value falls through to `coerce` which turns both the
    rational and the duration into floats.
    
    http://ruby-doc.org/core-2.2.0/Numeric.html#method-i-coerce

--- lib/rounding/time_extensions.rb
+++ lib/rounding/time_extensions.rb
@@ -1,5 +1,6 @@
 module Rounding::TimeExtensions
   def floor_to(step, around=-utc_offset)
+    step = step.to_r
     around = around.to_r
     rational_self = self.to_r
     difference = rational_self - rational_self.floor_to(step, around)
@@ -7,6 +8,7 @@ module Rounding::TimeExtensions
   end
 
   def ceil_to(step, around=-utc_offset)
+    step = step.to_r
     around = around.to_r
     rational_self = self.to_r
     difference = rational_self - rational_self.ceil_to(step, around)
@@ -14,6 +16,7 @@ module Rounding::TimeExtensions
   end
 
   def round_to(step, around=-utc_offset)
+    step = step.to_r
     around = around.to_r
     rational_self = self.to_r
     difference = rational_self - rational_self.round_to(step, around)
